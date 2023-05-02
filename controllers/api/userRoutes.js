4c4af6b882f3afdecdfed8b4a2606b11a0d235e5
AJoelj84 <ajoelj84@gmail.com>
2023-05-01 20:06:36 -0400

    Added more code to userRoutes.js, changed as well to accomodate login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,13 +1,41 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+const { User } = require('../../models');
 
 // Login Landing Page API routes
 router.get('/', (req, res) => {
   res.send('Welcome to the OnlyCheese* Login Page!');
 });
 
-router.post('/login', (req, res) => {
-  res.send('Logged in successfully!');
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // Cheesfolio Page API routes
@@ -34,4 +62,15 @@ router.get('/chat-room', (req, res) => {
 router.get('/newCheese', (req, res) => {
   res.send('Welcome to the OnlyCheese* Try a new Cheese Today!');
 });
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 module.exports = router;
