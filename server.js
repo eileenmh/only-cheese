1e23d99a9cfa1916ba8fd56c01643e7959bfecb2
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-05-01 21:39:07 -0400

    Add packages to node modules

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
     console.log(
-      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+      `\nServer running on port ${PORT}. Visit http://localhost:${PORT}!`
     )
   );
 });
