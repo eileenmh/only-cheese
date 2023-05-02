c9fd867c484c66fec6ddd683bd4bdc8ab18ee8a8
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-05-01 21:54:15 -0400

    update some handlebar files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,6 @@
 const router = require("express").Router();
+const userRoutes = require("./user-routes");
 
-const apiRoutes = require("./api");
-const homeRoutes = require("./home-routes.js");
-
-router.use("/", homeRoutes);
-router.use("/api", apiRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
