411bbc64f97c8bd7d67dc1faa6e352e252583a00
taylor-green <taylor.green2804@gmail.com>
2023-05-01 21:45:33 -0400

    started auth process, created auth.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const withAuth = require('../utils/auth')
 
 // Home Page API route
 router.get('/', (req, res) => {
   res.send('Welcome to OnlyCheese* Home Page!');
+});
+
+router.get('/login', (req, res) => {
+  res.send('Welcome to the Login Page!');
+});
+
+router.get('/cheesefolio', withAuth, (req, res) => {
+  res.send('This is your Cheesefolio!');
 });
 
 module.exports = router;
