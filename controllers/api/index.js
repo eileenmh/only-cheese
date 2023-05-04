8ef58e5ae1a73a1915ea345d573ab0620a521be7
taylor-green <taylor.green2804@gmail.com>
2023-05-03 20:33:31 -0400

    added cheesedate handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,18 @@
 const router = require("express").Router();
-const userRoutes = require("./user-routes");
+const userRoutes = require("./userRoutes");
+
+const handlebars = require('handlebars');
+
+// Assume we have an array of user objects called `users`
+
+const template = handlebars.compile(fs.readFileSync('cheese-date.handlebars', 'utf8'));
+const html = template({ users });
+
+
+
 
 router.use("/users", userRoutes);
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const routes = require("./controllers");
 const exphbs = require("express-handlebars");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
