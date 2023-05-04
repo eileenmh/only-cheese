4f0d297c0a2da08e257c49dd6054eddc2a23028a
taylor-green <taylor.green2804@gmail.com>
2023-05-04 17:11:58 -0400

    user model

--- models/user.js
+++ models/user.js
@@ -1,45 +1,42 @@
-const {Model, DataTypes} = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class User extends Model {}
 
 User.init(
-    {
-    user_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    email: { 
-        type: DataTypes.STRING
-    }, 
-    password:{
-        type: DataTypes.STRING
+  {
+    // Manually define the primary key
+    book_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
     },
     name: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING
+    },
+    password: {
+      type: DataTypes.STRING
     },
     bio: {
-        type: DataTypes.TEXT
+      type: DataTypes.TEXT
+    },
+    city: {
+      type: DataTypes.STRING
+    },
+    state: {
+      type: DataTypes.STRING
     },
     user_image: {
         type: DataTypes.STRING
-    },
-    cheeses: {
-        type: DataTypes.STRING
-    },
-    city: {
-        type: DataTypes.STRING
-    },
-    state: {
-        type: DataTypes.STRING
     }
-},
-{
+  },
+  {
     sequelize,
-    modelName:'User'
-    
-}
+    modelName: 'user'
+  }
 );
 
 module.exports = User;
