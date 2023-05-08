244ee31e8be1284a7bbc5e011d773e9b6daf476a
Damar Ellis <damar.ellis22@gmail.com>
2023-05-08 19:44:47 -0400

    Merge pull request #62 added City and State to user data

--- seeds/userData.js
+++ seeds/userData.js
@@ -4,26 +4,36 @@ const userData = [
   {
     name: "Sal",
     email: "sal@hotmail.com",
+    city: "Austin",
+    state: "TX",
     password: "password12345",
   },
   {
     name: "Lernantino",
     email: "lernantino@gmail.com",
+    city: "Austin",
+    state: "TX",
     password: "password12345",
   },
   {
     name: "Amiko",
     email: "amiko2k20@aol.com",
+    city: "Austin",
+    state: "TX",
     password: "password12345",
   },
   {
     name: "Jordan",
     email: "jordan99@msn.com",
+    city: "Austin",
+    state: "TX",
     password: "password12345",
   },
   {
     name: "Blake",
     email: "the_blake@yahoo.com",
+    city: "Austin",
+    state: "TX",
     password: "password12345",
   },
 ];
