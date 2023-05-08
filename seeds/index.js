4575bed8ccd1d50d709e5751082570bd41906498
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-05-08 18:02:14 -0400

    update seed files

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,12 @@
 const sequelize = require("../config/connection");
-const seedCheese = require("./CheeseData");
+const seedCheese = require("./cheeseData");
+const seedUser = require("./userData");
 
 const seedAll = async () => {
-  await sequelize.sync({ force: true });
+  await sequelize.sync({ force: true, logging: true });
 
   await seedCheese();
+  await seedUser();
   process.exit(0);
 };
 
