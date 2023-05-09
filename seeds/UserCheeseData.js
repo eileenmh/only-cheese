3bc1ae9edafb3cf709cdef45f28f60a651f4efce
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-05-09 13:09:44 -0400

    update data in UserCheeseData.js

--- seeds/UserCheeseData.js
+++ seeds/UserCheeseData.js
@@ -1,22 +1,55 @@
 const { UserCheese } = require("../models");
 
-const userCheeseData = [
-  {
-    user_id: 1,
-    cheese_id: 7,
-  },
-  {
-    user_id: 1,
-    cheese_id: 6,
-  },
-  {
-    user_id: 1,
-    cheese_id: 4,
-  },
-];
+const seedUserCheese = async function () {
+  await UserCheese.create(
+    {
+      user_id: 1,
+      cheese_id: 17,
+    },
+    {
+      fields: ["user_id", "cheese_id"],
+    }
+  );
 
-const seedUserCheese = UserCheese.bulkCreate(userCheeseData, {
-  fields: ["user_id", "cheese_id"],
-});
+  await UserCheese.create(
+    {
+      user_id: 2,
+      cheese_id: 16,
+    },
+    {
+      fields: ["user_id", "cheese_id"],
+    }
+  );
+
+  await UserCheese.create(
+    {
+      user_id: 3,
+      cheese_id: 6,
+    },
+    {
+      fields: ["user_id", "cheese_id"],
+    }
+  );
+
+  await UserCheese.create(
+    {
+      user_id: 4,
+      cheese_id: 14,
+    },
+    {
+      fields: ["user_id", "cheese_id"],
+    }
+  );
+
+  await UserCheese.create(
+    {
+      user_id: 5,
+      cheese_id: 15,
+    },
+    {
+      fields: ["user_id", "cheese_id"],
+    }
+  );
+};
 
 module.exports = seedUserCheese;
