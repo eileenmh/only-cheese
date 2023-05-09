02569acd32b444762065d22d8e7872ce8e9733bf
Damar Ellis <damar.ellis22@gmail.com>
2023-05-08 20:57:35 -0400

    Added more cheeses and added the bio to user data

--- seeds/cheeseData.js
+++ seeds/cheeseData.js
@@ -4,7 +4,16 @@ const cheeseData = [
   { name: "Manchego" },
   { name: "Blue cheese" },
   { name: "Raclette du Valais" },
-  { name: ""}
+  { name: "Taleggio" },
+  { name: "Cotija" },
+  { name: "Monterey Jack"},
+  { name: "Gorgonzola"},
+  { name: "Asiago"},
+  { name: "Mozzarella"},
+  { name: "Emmental"},
+  { name: "Pecorino Toscano"},
+  { name: "Cheddar"},
+  { name: "Gouda"},
 ];
 
 const seedCheese = () => Cheese.bulkCreate(cheeseData);
